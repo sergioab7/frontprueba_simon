7c87c1a56b83b3bcfbd042deebf430b0c6673d0c
sergioab7 <sergioandujar7@gmail.com>
2024-03-17 23:08:16 +0100

    cambiando uirls

---
+++ config.js
@@ -0,0 +1,3 @@
+export const URL = 'http://viaduct.proxy.rlwy.net:37388/api/dashboard'
+export const URL_LOGIN = 'http://viaduct.proxy.rlwy.net:37388/auth/login'
+export const URL_REGISTER = 'http://viaduct.proxy.rlwy.net:37388/auth/register'
